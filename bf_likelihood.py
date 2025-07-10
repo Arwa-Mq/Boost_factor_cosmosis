2bd222c20082142ce41826f5a35c2d3f0bcdcd3c
Arwa Qadi <76020791+Arwa-Mq@users.noreply.github.com>
2025-07-10 14:29:57 +0100

    Update bf_likelihood.py

--- bf_likelihood.py
+++ bf_likelihood.py
@@ -1,11 +1,24 @@
 # Arwa's likelihood. 
+
 import numpy as np
 from cosmosis.datablock import names
-import boost_factor_util as util
 
+def Boost_Factor_Model(R, rs, b0):
+    x = R / rs
+    fx = np.zeros_like(x)
+    fx[x > 1] = np.arctan(np.sqrt(x[x > 1]**2 - 1)) / np.sqrt(x[x > 1]**2 - 1)
+    fx[x == 1] = 1
+    fx[x < 1] = np.arctanh(np.sqrt(1 - x[x < 1]**2)) / np.sqrt(1 - x[x < 1]**2)
+    #fix the warning error
+    denominator = x**2 - 1
+    denominator[denominator == 0] = 1e-10  # or some small value
+    B = 1 + b0 * (1 - fx) / denominator
+    B[np.isnan(B)] = (b0 + 3) / 3
+    return B
+    
 def setup(options):
     # Reading data file:
-    # Change l and z according to which data file you are using
+    # DO: change l and z according to which data file you are using
     path = "/global/cfs/cdirs/des/jesteves/data/boost_factor/y1/profiles"
     R, data_vector, sigma_B = np.genfromtxt(path + "/full-unblind-v2-mcal-zmix_y1clust_l{l}_z{z}_zpdf_boost.dat".format(l=3, z=0), 
         unpack=True)
@@ -32,14 +45,13 @@ def setup(options):
 
     return config
 
-
 def execute(block, config):
     R = config['R']
     data_vector = config['data_vector']
     inv_cov = config['inv_cov']
 
     # Read parameter values from the block
-    # Change l and z according to which data file you are using, also do that in the bf_values.ini file. 
+    # DO: change l and z according to which data file you are using, also do that in the bf_values.ini file. 
     logrs = block["Boost_Factor_Model_Values", "logrs_30"]
     logb0 = block["Boost_Factor_Model_Values", "logb0_30"]
 
@@ -47,7 +59,7 @@ def execute(block, config):
     b0 = 10**logb0
 
     # Compute model prediction at current parameter values
-    model_prediction = util.Boost_Factor_Model(R, rs, b0)
+    model_prediction = Boost_Factor_Model(R, rs, b0)
 
     diff = model_prediction - data_vector
     # Chi-squared using covariance
